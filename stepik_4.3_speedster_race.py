eacbfcbb6d3111a300da69eea2d380ea470fff4f
whypxralich <whypxrlch@gmail.com>
2023-12-20 00:04:37 +0300

    Create stepik_4.3_speedster_race.py

---
+++ stepik_4.3_speedster_race.py
@@ -0,0 +1,11 @@
+#stepik_4.3_speedster_race
+
+zoom = int(input())
+flash = int(input())
+
+if zoom > flash:
+    print ("NO")
+elif zoom < flash:
+    print ("YES")
+elif zoom == flash:
+    print ("Don't know")
